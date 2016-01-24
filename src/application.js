7c26b722a5991f782d08021e200d18ab8dd82585
Luke <lukemorton.dev@gmail.com>
2016-01-24 11:56:13 +0000

    Tidy up

--- src/application.js
+++ src/application.js
@@ -2,6 +2,15 @@ import App from './views/application';
 import browserify from 'browserify';
 import fs from 'fs';
 
+const browserifyOptions = {
+  standalone: 'app',
+  insertGlobalVars: {
+    React: function(file, dir) {
+      return 'require("react")';
+    }
+  }
+};
+
 function buildIndex(rootPath) {
   const indexPath = __dirname + '/../index.js';
   console.log('Writing index to', indexPath);
@@ -9,27 +18,20 @@ function buildIndex(rootPath) {
   return indexPath;
 }
 
-export function buildApp({ config, onFinish }) {
+function serverBundleStream(config, onFinish) {
   console.log('Building app at', config.app.rootPath);
 
-  const outputStream = fs.createWriteStream('bundle.js')
+  return fs.createWriteStream('bundle.js')
     .on('finish', function() {
       console.log('Finished building app.');
       onFinish(App({ app: require('../bundle').default, config }));
     });
+}
 
-  const browserifyOptions = {
-    standalone: 'app',
-    insertGlobalVars: {
-      React: function(file, dir) {
-        return 'require("react")';
-      }
-    }
-  };
-
+export function buildApp({ config, onFinish }) {
   browserify(buildIndex(config.app.rootPath), browserifyOptions)
     .transform('babelify', { presets: ['es2015', 'react'] })
     .transform('bulkify')
     .bundle()
-    .pipe(outputStream);
+    .pipe(serverBundleStream(config, onFinish));
 }
