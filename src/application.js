82400e8d9eb61f6905045eef84654e6024602e66
Luke <lukemorton.dev@gmail.com>
2016-01-24 12:50:57 +0000

    Change configuration to set paths at current working directory

--- src/application.js
+++ src/application.js
@@ -11,25 +11,28 @@ const browserifyOptions = {
   }
 };
 
-function buildIndex(rootPath) {
-  const indexPath = __dirname + '/../index.js';
+function buildIndex(config) {
+  const indexPath = config.app.tmpPath + '/index.js';
   console.log('Writing index to', indexPath);
-  fs.writeFileSync(indexPath, `export default require('bulk-require')('${rootPath}', ['views/**/*.jsx']);`);
+  fs.writeFileSync(indexPath, `export default require('bulk-require')('${config.app.rootPath}', ['views/**/*.jsx']);`);
   return indexPath;
 }
 
 function serverBundleStream(config, onFinish) {
+  const bundlePath = config.app.tmpPath + '/bundle.js';
   console.log('Building app at', config.app.rootPath);
 
-  return fs.createWriteStream('bundle.js')
+  return fs.createWriteStream(bundlePath)
     .on('finish', function() {
       console.log('Finished building app.');
-      onFinish(App({ app: require('../bundle').default, config }));
+      onFinish(App({ app: require(bundlePath).default, config }));
     });
 }
 
 export function buildApp({ config, onFinish }) {
-  browserify(buildIndex(config.app.rootPath), browserifyOptions)
+  if (!fs.existsSync(config.app.tmpPath)) fs.mkdirSync(config.app.tmpPath);
+
+  browserify(buildIndex(config), browserifyOptions)
     .transform('babelify', { presets: ['es2015', 'react'] })
     .transform('bulkify')
     .bundle()
--- src/configuration.js
+++ src/configuration.js
@@ -1,11 +1,20 @@
 import path from 'path';
 
-export function loadConfig(env) {
-  return {
-    app: { assetPath: '',
-           rootPath: path.resolve(`${__dirname}/../examples/app/`),
-           viewPath: path.resolve(`${__dirname}/../examples/app/views/`) },
-    express: { serveStatic: false },
-    port: 3000
+function defaultConfig() {
+  const app = {
+    assetsPath: path.resolve(process.cwd() + '/app/assets/'),
+    tmpPath: path.resolve(process.cwd() + '/tmp/'),
+    rootPath: path.resolve(process.cwd() + '/app/'),
+    viewsPath: path.resolve(process.cwd() + '/app/views/')
   };
+
+  const express = {
+    serveStatic: false
+  };
+
+  return { port: 3000, app, express };
+}
+
+export function loadConfig(env) {
+  return defaultConfig();
 }
