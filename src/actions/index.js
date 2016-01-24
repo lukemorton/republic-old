6cbdc353d48a9df50a7c404e8f18ad164c4638d1
Luke <lukemorton.dev@gmail.com>
2016-01-24 15:04:28 +0000

    Remove example action

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,12 +6,4 @@ import { version } from '../package';
 program
   .version(version)
   .parse(process.argv)
-  .command('dev')
-  .action(function (dir, otherDirs) {
-    console.log('rmdir %s', dir);
-    if (otherDirs) {
-      otherDirs.forEach(function (oDir) {
-        console.log('rmdir %s', oDir);
-      });
-    }
-  });
+  .command('dev');
