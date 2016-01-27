ba3ff9897b6a124dc536c79971c85a5fdd6904ad
Luke <lukemorton.dev@gmail.com>
2016-01-27 21:40:24 +0000

    Remove layout wrapper

--- examples/config/routes.jsx
+++ examples/config/routes.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
 export default (
-  <Route path="/" component={({ children }) => children}>
+  <Route path="/">
     <IndexRoute component={(props) => <div>Hello World</div>} />
     <Route path="test" component={(props) => <div>Test</div>} />
   </Route>
