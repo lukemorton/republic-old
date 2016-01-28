ace39ce9aa0e44125a5aaeca29fae2144ae2b876
Luke <lukemorton.dev@gmail.com>
2016-01-28 21:25:58 +0000

    Remove unused file

--- src/router.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'react-router';
-
-function loadRoutes(config) {
-  // config.app.configPath
-}
-
-export default function ({ config, history }) {
-  return Router({ history }, loadRoutes(config));
-}
