e34403e6601a4703f52443ea616a13c2de4758c9
Luke <lukemorton.dev@gmail.com>
2016-01-31 23:22:26 +0000

    Add deep merging of state

--- src/store.js
+++ src/store.js
@@ -1,7 +1,11 @@
 import * as Redux from 'redux';
+import merge from 'deepmerge';
 
 export function createStore(config) {
-  return Redux.createStore(function () {
-    return {};
+  return Redux.createStore(function (state = {}, action) {
+    switch (action.type) {
+      case 'MERGE_STATE': return merge(state, action.state);
+      default: return state;
+    }
   });
 }
