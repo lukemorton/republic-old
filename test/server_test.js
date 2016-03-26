8e439fd918b16b75256462caa59578500b28d383
Luke <lukemorton.dev@gmail.com>
2016-03-26 01:19:10 +0000

    Increase timeout for travis

--- test/server_test.js
+++ test/server_test.js
@@ -3,7 +3,7 @@ import { run } from '../src/server';
 describe('Server', function () {
   context('when retrieving homepage', function () {
     it('should return 200', function (done) {
-      this.timeout(20000);
+      this.timeout(40000);
       const env = 'test';
       const rootDir = 'examples';
 
