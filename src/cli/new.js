fa37e7186402d5209239f050e61c899d24349c61
Luke <lukemorton.dev@gmail.com>
2016-03-27 17:58:52 +0100

    Improve republic new command

--- src/cli/new.js
+++ src/cli/new.js
@@ -1,18 +1,34 @@
 #!/usr/bin/env node
 
 import program from 'commander';
-import { exec } from 'child_process';
+import { exec, spawn } from 'child_process';
+import { loadConfig } from '../configuration';
+import { createLogger } from '../logging';
+
+console.log(process.env.NODE_ENV);
 
 program.arguments('<cmd> [dir]')
   .parse(process.argv);
 
 const exampleDir = __dirname + '/../../examples/';
 const [appDir] = program.args;
+const logger = createLogger({ config: loadConfig({ env: process.env.NODE_ENV }) });
 
-console.log('Creating new application inside', appDir);
+logger.info(`Creating new application inside ${appDir}...`);
 
 exec('cp -r ' + exampleDir + ' ' + appDir, function () {
-  exec('npm install', { cwd: appDir }, function () {
-    console.log('Now change into', appDir, 'and run republic dev');
+  logger.info('Installing dependencies...');
+
+  const patience = setInterval(_ => logger.info('Still installing...'), 10000);
+
+  exec('npm set progress=false && npm install', { cwd: appDir }, function () {
+    clearInterval(patience);
+    logger.info('Application ready.');
+    console.log('');
+    logger.info('Run the following to start a development server:');
+    console.log('');
+    logger.info('    cd', appDir);
+    logger.info('    republic dev');
+    console.log('');
   });
 });
