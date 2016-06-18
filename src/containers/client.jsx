b162d31bd178889e63f7059f025aa3622c46f24d
Luke <lukemorton.dev@gmail.com>
2016-06-18 11:58:54 +0100

    Remove history as dep as now provided by react-router

--- src/containers/client.jsx
+++ src/containers/client.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { Router } from 'react-router';
+import { Router, browserHistory } from 'react-router';
 import { createRoutes } from '../routing';
-import createBrowserHistory from 'history/lib/createBrowserHistory';
 
 export default function ({ appTree, store }) {
   const routes = createRoutes({ appTree, store });
 
   return (
     <Provider store={store}>
-      <Router history={createBrowserHistory()}>
+      <Router history={browserHistory}>
         {routes}
       </Router>
     </Provider>
